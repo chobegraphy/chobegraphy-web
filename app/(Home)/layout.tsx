6a96e7ce2f64d5ce88bf316d4a68ecb33bc13406
arafatrahat94 <arafatrahat94@gmail.com>
2025-01-31 18:21:53 +0600

    setUp push

---
+++ app/(Home)/layout.tsx
@@ -0,0 +1,9 @@
+const HomeLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <div>{children}</div>;
+};
+
+export default HomeLayout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +10,17 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        RIGHT: ["Righteous", "sans-serif"],
+        JSF: ["Josefin Sans", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primaryColor: "#2CAB00",
+        secondaryColor: "#9CD788",
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
