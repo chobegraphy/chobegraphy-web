50067960bff958456bf1ec5fb03c84b22babd56b
arafatrahat94 <arafatrahat94@gmail.com>
2025-02-17 01:14:32 +0600

    fix

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
