451dc11037ea4b8650906a95f65cf35f497f3c8a
arafatrahat94 <arafatrahat94@gmail.com>
2025-02-19 16:18:56 +0600

    fix issue

--- src/app/(Home)/layout.tsx
+++ src/app/(Home)/layout.tsx
@@ -1,31 +1,33 @@
 "use client";
 import Footer from "@/components/shared/Footer/Footer";
 import Navbar from "@/components/shared/Navbar/Navbar";
+import LoadingAnimation from "@/components/ui/loadingAnimation/loadingAnimation";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import AuthProvider from "../../../Provider/AuthProvider";
 import { store } from "../../../Redux/Store";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const pathName = usePathname();
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 2000);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <Provider store={store}>
       <AuthProvider>
-        {/* {loading && <LoadingAnimation />}
+        {loading && <LoadingAnimation />}
         {!loading && (
           <>
-          </>
-        )} */}
-        {pathName !== "/SignUp" && pathName !== "/SignIn" && <Navbar />}
-        {children}
+            {pathName !== "/SignUp" && pathName !== "/SignIn" && <Navbar />}
+            {children}
 
-        {pathName !== "/SignUp" && pathName !== "/SignIn" && <Footer />}
+            {pathName !== "/SignUp" && pathName !== "/SignIn" && <Footer />}
+          </>
+        )}
       </AuthProvider>
     </Provider>
   );
